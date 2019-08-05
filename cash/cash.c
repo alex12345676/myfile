1be95241958639c2fec36e5a61aec54cd2426650
Александр <52318452+alex12345676@users.noreply.github.com>
2019-08-05 12:44:49 +0600

    Update cash.c

--- cash/cash.c
+++ cash/cash.c
@@ -1,1 +1,21 @@
 #include<stdio.h>
+char Input[256];
+float chartofloat(char *s)
+{
+  float res=0;
+  int sign=1;
+  if((s[0]>=0)&&(s[0]<=9)||(s[0]==',')||(s[0]=='-'))
+  {
+    
+}
+float get_float(char *message)
+{
+  printf("%s",massage);
+  gets(&Input);
+  return chartofloat(Input);
+}
+
+int main()
+{
+  
+}
