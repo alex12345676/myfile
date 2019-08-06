a9a67c0de7e4fd24d7727eeb44295ceaffdf8313
Александр <schurik_07@mail.ru>
2019-08-06 18:14:58 +0000

    updat cash

--- cash/cash.c
+++ cash/cash.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<math.h>
 
-char Input[256];
+char [256];
 int strlen(char *s)
 {
   int res=0;
@@ -64,10 +64,27 @@ float get_float(char *message)
 int main()
 {
   float f=get_float("input float:  ");
-  int ten_cent,fiften_cent,all_cents,cent;
+  int ten_cent,fiften_cent,all_cents,cent,rub,fif_rub,ten_rub,tause_rub,fif_tause_rub,fiften_rub,sto_rub,dvest_rub,all_rub,patsot_rub,tu_rub;
   all_cents=(int)round(f*100);
+  all_rub=all_cents/100;
+  fif_tause_rub=all_rub/5000;
+  tause_rub=all_rub%5000/1000;
+  patsot_rub=all_rub%5000%1000/500;
+  dvest_rub=all_rub%5000%1000%500/200;
+  sto_rub=all_rub%5000%1000%500%200/100;
+  fiften_rub=all_rub%5000%1000%500%200%100/50;
+  ten_rub=all_rub%5000%1000%500%200%100%50/10;
+  fif_rub=all_rub%5000%1000%500%200%100%50%10/5;
+  tu_rub=all_rub%5000%1000%500%200%100%50%10%5/2;
+  rub=all_rub%5000%1000%500%200%100%50%10%5%2;
+  all_cents-=(all_rub*100);
   fiften_cent=all_cents/50;
   ten_cent=all_cents%50/10;
   cent=all_cents%50%10;
+  printf("                    bumazhnie\n");
+  printf(" 5000-: %i shtuk, 1000-: %i shtuk, 500-: %i shtuk\n 200-: %i shtuk, 100-: %i shtuk, 50-: %i shtuk\n",
+        fif_tause_rub,tause_rub,patsot_rub,dvest_rub,tu_rub,fiften_rub);
+  printf("                    monete\n");
+  printf(" 10rub - %i shtuk, 5rub - %i shtuk, 2rub - %i shtuk, 1rub - %i shtuk\n",ten_rub,fif_rub,tu_rub,rub);
   printf("50 kopeek: %i shtuk; 10 kopeek: %i shtuk; 1 kopeek: %i shtuk\n",fiften_cent,ten_cent,cent) ;
 }
